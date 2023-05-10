8f214a245a6ccdddb842693f3d6ce74ef1c6cd31
AIE Staff Temp <adam.schmidt@aie.edu.au>
2023-05-11 09:15:48 +0930

    Addition

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -3,18 +3,23 @@
 
 #include <iostream>
 
+float Add(float x, float y);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Inputs
+    float x, y;
+    std::cout << "I am a calculator!\nEnter your numbers...\n";
+    std::cout << "X: ";
+    std::cin >> x;
+    std::cout << "Y: ";
+    std::cin >> y;
+
+    //Calculations
+    std::cout << x << " + " << y << " = " << Add(x, y);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+float Add(float x, float y)
+{
+    return x + y;
+}
